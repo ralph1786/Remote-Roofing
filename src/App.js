786eb7d1c89ec88515eac74757c703a48f10f273
ralph1786 <ralph1786@gmail.com>
2020-05-28 16:09:20 -0400

    added footer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./App.scss";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <h2>Hello World</h2>
+      <Footer />
     </div>
   );
 }
