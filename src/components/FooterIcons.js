d481eee03665cb4a30ab968ed59cf01d75b4c219
ralph1786 <ralph1786@gmail.com>
2020-05-28 22:25:29 -0400

    added new download icon

--- src/components/FooterIcons.js
+++ src/components/FooterIcons.js
@@ -7,21 +7,32 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 import AppleStore from "../Assets/icons/apple-store.svg";
 import GoogleStore from "../Assets/icons/google-play.svg";
+import Microsoft from "../Assets/icons/microsoftBadge.svg";
+import "./FooterIcons.scss";
 
 function FooterIcons() {
+  const socialMediaIconsImages = [faFacebookF, faTwitter, faInstagram];
+  const socialMediaIcons = socialMediaIconsImages.map((icon, index) => (
+    <FontAwesomeIcon icon={icon} size="2x" key={index} />
+  ));
+
   return (
-    <div>
-      <FontAwesomeIcon icon={faFacebookF} size="2x" />
-      <FontAwesomeIcon icon={faTwitter} size="2x" />
-      <FontAwesomeIcon icon={faInstagram} size="2x" />
-      <img
-        src={AppleStore}
-        alt="logo to download application from apple store"
-      />
-      <img
-        src={GoogleStore}
-        alt="logo to download application from google play store"
-      />
+    <div className="container-icons">
+      <div className="social-media-icons">{socialMediaIcons}</div>
+      <div className="download-icons">
+        <img
+          src={AppleStore}
+          alt="logo to download application from apple store"
+        />
+        <img
+          src={GoogleStore}
+          alt="logo to download application from google play store"
+        />
+        <img
+          src={Microsoft}
+          alt="logo to download application from Microsoft"
+        />
+      </div>
     </div>
   );
 }
