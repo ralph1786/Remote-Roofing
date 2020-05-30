a83f787f6482ef3be5a7d6a03b79a2bad78ecbf9
ralph1786 <ralph1786@gmail.com>
2020-05-30 00:42:50 -0400

    added tiles

---
+++ src/Pages/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Series from "../Assets/images/series.png";
+import Movies from "../Assets/images/movies.png";
+import ProductTile from "../components/ProductTile";
+import "./Home.scss";
+
+function Home() {
+  return (
+    <div className="container-home">
+      <Link to="/series">
+        <ProductTile image={Series} title="Popular Series" />
+      </Link>
+      <Link to="/movies">
+        <ProductTile image={Movies} title="Popular Movies" />
+      </Link>
+    </div>
+  );
+}
+
+export default Home;
