549e20a0bfd2d235ff812c694920eed1211a209f
ralph1786 <ralph1786@gmail.com>
2020-05-31 16:42:14 -0400

    added routing and components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Home from "./Pages/Home";
+import Series from "./Pages/Series";
+import Movies from "./Pages/Movies";
+import { Route, Switch, withRouter } from "react-router-dom";
 import "./App.scss";
 
-function App() {
+function App({ location }) {
   return (
     <div className="App">
-      <Header />
-      <h2>Hello World</h2>
-      <Footer />
+      <Header location={location} />
+      <Switch>
+        <Route path="/series" render={() => <Series />} />
+        <Route path="/movies" render={() => <Movies />} />
+        <Route path="/" render={() => <Home />} />
+      </Switch>
+      <Footer location={location} />
+      <p>Learn React</p>
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
