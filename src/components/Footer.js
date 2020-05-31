25d306724b9a54de5fc303f332cd40e52f77c701
ralph1786 <ralph1786@gmail.com>
2020-05-31 16:45:14 -0400

    added location prop and copyright msg

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,14 @@ import FooterIcons from "./FooterIcons";
 import FooterLinks from "./FooterLinks";
 import "./Footer.scss";
 
-function Footer() {
+function Footer({ location }) {
   return (
-    <div className="footer">
+    <div
+      className="footer"
+      style={{ position: location.pathname === "/" ? "absolute" : "relative" }}
+    >
       <FooterLinks />
+      <p>Copyright &copy; 2016 DEMO Streaming All Rights Reserved.</p>
       <FooterIcons />
     </div>
   );
