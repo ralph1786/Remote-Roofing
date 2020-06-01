5006389063f5f3dd818f1fdee1c32edf93750ed4
ralph1786 <ralph1786@gmail.com>
2020-06-01 18:20:14 -0400

    changed import paths

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import App from "./App";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import { shallow } from "enzyme";
 
 let wrapper;
